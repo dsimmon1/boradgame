55e6c701b106d20016b5f13e8259e1d4a817567f
dsimmon1 <simmons.diana93@gmail.com>
2018-04-07 19:46:00 -0700

    changed the contact page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
@@ -8,7 +8,7 @@ const request = require("request");
 
 const app = express();
 
-let PORT = process.env.PORT || 3001;
+let PORT = process.env.PORT || 2002;
 
 
 app.set('views', __dirname + '/views');
@@ -85,37 +85,17 @@ app.post("/contact", function(req, res) {
   `
   console.log(output);
 
-      let transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user: "simmons.diana93@gmail.com",
-            pass: "M!sCMBr0wn4Ever!"
-        }
-    });
+  sgMail.setApiKey('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');
+const msg = {
+  to: 'simmons.diana93@gmail.com',
+  from: 'simmons.diana93@gmail.com',
+  subject: req.body.name + ' sent you a new messgae',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: output
+};
+sgMail.send(msg);
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Diana Simmons" <simmons.diana93@gmail.com>', // sender address
-        // to: 'iamariellesimonee@gmail.com',
-        // to: 'moonlitmoorgames@gmail.com',
-        to: 'simmons.diana93@gmail.com',
-        subject: 'New Contact from Website', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
+     
     });
 
 app.post("/newsletter", function(req, res) {
